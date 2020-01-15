e116a1813b903cbc9809ae03428b386064ac8994
Cristobal-Guillen <cristobalguillen47@gmail.com>
2020-01-15 16:42:55 +0100

    he empezado a modificar la rama prueba1

--- holamundoc++.cpp
+++ holamundoc++.cpp
@@ -8,6 +8,10 @@ int main() {
     cout << "Hola clase" << endl;
     cout << "Hola clase" << endl;
     cout << "Hola hola" << endl;
+    cout << "Hola m" << endl;
+    cout << "Hola u" << endl;
+    cout << "Hola n" << endl;
+    cout << "Hola do" << endl;
     return 0;
 
 }
